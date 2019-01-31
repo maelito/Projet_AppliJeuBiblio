2a6e50a02272fc62d7310b9381033923d6492f22
Maël CLEMENT <maelclement.pro@gmail.com>
2019-01-31 11:47:05 +0100

    activity_theme images

--- app/src/main/java/com/example/mclement/applijeubiblio/Regles.java
+++ app/src/main/java/com/example/mclement/applijeubiblio/Regles.java
@@ -1,14 +1,16 @@
 package com.example.mclement.applijeubiblio;
 
 import android.app.AppComponentFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Regles extends AppCompatActivity {
     TextView regles;
-    Button retour;
+    Button btnRetour;
     String txtRegles;
 
     @Override
@@ -17,14 +19,32 @@ public class Regles extends AppCompatActivity {
         setContentView(R.layout.activity_regles);
 
         regles = (TextView) findViewById(R.id.txtRegles);;
+        btnRetour = findViewById(R.id.retour);
+
 
         txtRegles = "Les règles du Memory :" +
-                "\n \n - Il te faut trouver toutes les paires des différents personnages." +
-                "\n \n - Le but étant de le faire en moins de coups possible !" +
-                "\n \n - Tu as 8 paires à trouver ! " +
+                "\n \n Il te faut trouver toutes les paires des différents personnages." +
+                "\n \n Le but étant de le faire en moins de coups possible !" +
+                "\n \n Tu as 8 paires à trouver ! " +
                 "\n \n Le jeu s'arrête quand tu as trouvé toutes les paires !";
 
         regles.setText(txtRegles);
+
+        bouton();
+
     }
+    private void bouton()
+    {
+        //bouton retour qui fait revenir a la page d'acceuil
+        btnRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Menu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
 
 }
